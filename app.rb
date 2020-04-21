6385ab619fb12bbe5f8ef22144fa5798afbed299
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-21 21:07:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-     "Hello, World! Me dey ya Mi Say!!!"
+     "Hello, World! Me dey ya Mi Say!!!
+     Me ah tell yuh say Mi dey ya!!"
    end
 
 end
