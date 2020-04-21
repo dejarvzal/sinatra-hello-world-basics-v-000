aee70faa675a1877e49c8544a9fb3b4622a5ea41
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-21 21:09:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-     "Hello, World! 
-     If you have the ability to imagine it, or even to think about it, 
+     "Hello, World!
+     If you have the ability to imagine it, or even to think about it,
      this Universe has the ability and the resources to deliver it fully unto you!"
    end
 
