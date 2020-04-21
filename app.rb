bdef584925780bcc66ca2705174b813e21e139f9
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-21 21:50:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   get '/' do
      "Hello, World!
      If you have the ability to imagine it, or even to think about it,
-     this Universe has the ability and the resources to deliver it fully unto you!" <%=Time.now%>
+     this Universe has the ability and the resources to deliver it fully unto you!" '<%=Time.now%>'
     #  code = "<%=Time.now %>
     #  erb code
    end
